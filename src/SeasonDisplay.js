ceecf71659e5034213b532fd89ae3ff79aa442b9
GalGog <galynna00@gmail.com>
2019-02-15 17:30:31 +0000

    showing icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,11 +11,17 @@ if (month > 2 && month < 9 ) {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
 
-    console.log(season);
 
-
-    return <div>Season Display</div>;
+    return (
+    <div>
+        <i className={`${icon} icon`} />
+        <h1>{text}</h1>
+        <i className={`${icon} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
